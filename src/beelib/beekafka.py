35eaa0e1eae9b4fd450ae59e3b8b75692f4216ac
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2024-04-09 17:14:13 +0200

    corrected kafka encoders

--- src/beelib/beekafka.py
+++ src/beelib/beekafka.py
@@ -1,15 +1,25 @@
+import json
+
 from kafka import KafkaProducer, KafkaConsumer
 from base64 import b64encode, b64decode
 import pickle
 import sys
 
 
-def __json_decoder__(v):
+def __pickle_decoder__(v):
     return b64encode(pickle.dumps(v))
 
 
-def __json_encoder__(v):
+def __pickle_encoder__(v):
     return pickle.loads(b64decode(v))
+
+
+def __json_encoder__(v):
+    return json.dumps(v)
+
+
+def __json_decoder__(v):
+    return json.loads(v)
 
 
 def __plain_decoder_encoder(v):
@@ -19,8 +29,10 @@ def __plain_decoder_encoder(v):
 def create_kafka_producer(kafka_conf, encoding="JSON", **kwargs):
     if encoding == "PLAIN":
         encoder = __plain_decoder_encoder
+    elif encoding == "PICKLE":
+        encoder = __pickle_encoder__
     elif encoding == "JSON":
-        encoder = __json_decoder__
+        encoder = __json_encoder__
     else:
         raise NotImplementedError("Unknown encoding")
     servers = [f"{kafka_conf['host']}:{kafka_conf['port']}"]
@@ -30,8 +42,10 @@ def create_kafka_producer(kafka_conf, encoding="JSON", **kwargs):
 def create_kafka_consumer(kafka_conf, encoding="JSON", **kwargs):
     if encoding == "PLAIN":
         decoder = __plain_decoder_encoder()
+    elif encoding == "PICKLE":
+        decoder = __pickle_decoder__
     elif encoding == "JSON":
-        decoder = __json_encoder__
+        decoder = __json_decoder__
     else:
         raise NotImplementedError("Unknown encoding")
 
